4bca7f4dfff1e6173737f1fe8391d79dc07fd78e
Kiran Raut <kiranraut04196@gmail.com>
2025-05-22 19:59:03 +0530

    abhi bhi ye ui/ux improvement hi chalu hai

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import logo from "../assets/logo.webp";
-import { motion } from "motion/react";
 
 const Nav = () => {
   return (
-    <nav
-      className="flex h-20 items-center justify-center
-        gap-[10px] w-full shadow-[9px_3px_6px_1px_#0f0f0f99]"
-    >
-      <div
-        className="w-20 h-15 mr-0 justify-items-start"
-      >
+    <nav className="w-full h-20 bg-white shadow-[9px_3px_6px_1px_#0f0f0f99] flex items-center justify-center relative">
+      
+      {/* Centered text */}
+      <div className="text-xl md:text-3xl lg:text-3xl xl:text-2xl text-center">
+        STUDIO SAARTHI
+      </div>
+
+      {/* Logo in the corner */}
+      <div className="absolute left-5 top-1/2 -translate-y-1/2 w-16 h-16">
         <img
           src={logo}
           alt="logo"
-          className=" h-[100%] w-[100%] object-contain ml-5"
+          className="w-full h-full object-contain"
         />
-      </div>
-
-      <div
-        className=" text-xl
-            md:text-3xl lg:text-3xl xl:text-2xl"
-      >
-        STUDIO SAARTHI
       </div>
     </nav>
   );
